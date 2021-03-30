3666066722e20a9eabd2b8b866f00d39f63b75f0
CameronFarquhar <cameronfarquhar@gmail.com>
2021-03-30 14:55:48 -0400

    updated photo, changed header text color, removed previous table upon function execution

--- UFO_level_1/static/js/app.js
+++ UFO_level_1/static/js/app.js
@@ -1,35 +1,32 @@
-// from data.js
 var tableData = data;
 
-// YOUR CODE HERE!
-// Select the button
 var button = d3.select("#filter-btn");
 
-// Select the form
 var form = d3.select(".form-control");
 
 var tbody = d3.select("tbody");
 
-// Create event handlers 
 button.on("click", runEnter);
 form.on("submit",runEnter);
 
 function runEnter () {
+
     d3.event.preventDefault();
 
     var inputElement = d3.select("#datetime");
 
-    // Get the value property of the input element
     var inputValue = inputElement.property("value");
 
     var filteredData = tableData.filter(tableData => tableData.datetime === inputValue);
+
+    tbody.html("");
 
     filteredData.forEach((sightings) => {
         var row = tbody.append("tr");
         Object.entries(sightings).forEach(([key, value]) => {
           var cell = row.append("td");
           cell.text(value);
+          
         });
       });
 };
-
