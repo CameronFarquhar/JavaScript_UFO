e239d1478e216613faa2842c6feab74fa4f1b4c4
CameronFarquhar <cameronfarquhar@gmail.com>
2021-03-31 15:50:31 -0400

    filter by city

--- UFO_level_1/static/js/app.js
+++ UFO_level_1/static/js/app.js
@@ -1,29 +1,54 @@
 var tableData = data;
 
-var button = d3.select("#filter-btn");
-
-var form = d3.select(".form-control");
+var buttonDate = d3.select("#filter-btn-date");
 
 var tbody = d3.select("tbody");
 
-button.on("click", runEnter);
-form.on("submit",runEnter);
+buttonDate.on("click", runEnterDate);
 
-function runEnter () {
+
+function runEnterDate () {
 
     d3.event.preventDefault();
 
-    var inputElement = d3.select("#datetime");
+    var inputElementDate = d3.select("#datetime");
 
-    var inputValue = inputElement.property("value");
+    var inputValueDate = inputElementDate.property("value");
 
-    var filteredData = tableData.filter(tableData => tableData.datetime === inputValue);
+    var filteredData = tableData.filter(tableData => tableData.datetime === inputValueDate);
 
     tbody.html("");
 
     filteredData.forEach((sightings) => {
         var row = tbody.append("tr");
         Object.entries(sightings).forEach(([key, value]) => {
+          var cell = row.append("td");
+          cell.text(value);
+        });
+      });
+};
+
+
+var buttonCity = d3.select("#filter-btn-city");
+
+buttonCity.on("click", runEnter);
+
+
+function runEnter () {
+
+    d3.event.preventDefault();
+
+    var inputElementCity = d3.select("#city");
+
+    var inputValueCity = inputElementCity.property("value");
+
+    var filteredDataCity = tableData.filter(tableData => tableData.city === inputValueCity);
+
+    tbody.html("");
+
+    filteredDataCity.forEach((sightingsCity) => {
+        var row = tbody.append("tr");
+        Object.entries(sightingsCity).forEach(([key, value]) => {
           var cell = row.append("td");
           cell.text(value);
           
