8ca967df4695cc44ed99c8707eddc88b8aa482fd
CameronFarquhar <cameronfarquhar@gmail.com>
2021-04-05 22:05:54 -0400

    Final code completed and cleaned

--- UFO_level_2/static/js/app.js
+++ UFO_level_2/static/js/app.js
@@ -1,12 +1,10 @@
+// create variable to connect with data file
 var tableData = data;
 
+// create variables to interact with html
 var button = d3.select("#filter-btn");
 
 var tbody = d3.select("tbody");
-
-button.on("click", runEnter);
-
-// button.getElementById("#filter-btn").addEventListener("change", runEnter);
 
 var DateData = d3.select("#datetime");
 var City = d3.select("#city");
@@ -16,7 +14,11 @@ var Shape = d3.select("#shape");
 
 var errMsg = d3.select("#error-message")
 
+// create event listener
+button.on("click", runEnter);
 
+// this loop populates the table by grabbing th input values that match table data, 
+// then creates a row then inserts each cell with data
 function populateData(userEntry) {
   userEntry.forEach((sightings) => {
     var row = tbody.append("tr");
@@ -30,218 +32,46 @@ function populateData(userEntry) {
 
 function runEnter () {
 
-    // d3.event.preventDefault();
-    var filteredData = tableData;
+  // define variable paths to variables outside of function
+  var filteredData = tableData;
 
-    var inputValueDate = DateData.property("value");
+  var inputValueDate = DateData.property("value");
 
-    var inputValueCity = City.property("value").toLowerCase();
+  var inputValueCity = City.property("value").toLowerCase();
 
-    var inputValueState = State.property("value").toLowerCase();
+  var inputValueState = State.property("value").toLowerCase();
 
-    var inputValueCountry = Country.property("value").toLowerCase();
+  var inputValueCountry = Country.property("value").toLowerCase();
 
-    var inputValueShape = Shape.property("value").toLowerCase();
+  var inputValueShape = Shape.property("value").toLowerCase();
 
-if (inputValueCity) {
-  filteredData = filteredData.filter(tableData => tableData.city === inputValueCity);
-};
-if (inputValueCountry) {
-  filteredData = filteredData.filter(tableData => tableData.country === inputValueCountry);
-};
-if (inputValueDate) {
-  filteredData = filteredData.filter(tableData => tableData.datetime === inputValueDate);
-};
-if (inputValueState) {
-  filteredData = filteredData.filter(tableData => tableData.state === inputValueState);
-};
-if (inputValueShape) {
-  filteredData = filteredData.filter(tableData => tableData.shape === inputValueShape);
-};
+  // create a conditional statement that filters each respective column and modifies the filteredData variable
 
-    tbody.html("");
+  if (inputValueDate) {
+   filteredData = filteredData.filter(tableData => tableData.datetime === inputValueDate);
+  };
+  if (inputValueCity) {
+    filteredData = filteredData.filter(tableData => tableData.city === inputValueCity);
+  };
+  if (inputValueCountry) {
+    filteredData = filteredData.filter(tableData => tableData.country === inputValueCountry);
+  };
+  if (inputValueState) {
+    filteredData = filteredData.filter(tableData => tableData.state === inputValueState);
+  };
+  if (inputValueShape) {
+    filteredData = filteredData.filter(tableData => tableData.shape === inputValueShape);
+  };
 
-    // d3.select("p").remove();
+  // clear out body if/when no conditions are met.
+  tbody.html("");
 
-    if (filteredData !== 0) {
-      populateData(filteredData);
-    }
-  }
-      // else if (filteredData.length === 0) {
-      //   d3.select("tbody").selectAll("tr").remove();
+  // use the populateData function to loop through the data file where input conditions are met.
+  if (filteredData !== 0) {
+    populateData(filteredData);}
+      // else {
+      //   // d3.select("tbody").selectAll("tr").remove();
       //   errMsg.append("p").text("These are not the droids you are looking for, move along");
       //   };
 
-
-//     if (filteredDataCity !== 0) {
-//       populateData(filteredDataCity);
-//       }
-//     //   else if (filteredDataCity.length === 0) {
-//     //     d3.select("tbody").selectAll("tr").remove();
-//     //     errMsg.append("p").text("These are not the droids you are looking for, move along");
-//     // };
-
-
-//     if (filteredDataState !== 0) {
-//       populateData(filteredDataState);
-//       }
-//     //   else if (filteredDataState.length === 0) {
-//     //     d3.select("tbody").selectAll("tr").remove();
-//     //     errMsg.append("p").text("These are not the droids you are looking for, move along");
-//     // };
-
-
-//     if (filteredDataCountry !== 0) {
-//       populateData(filteredDataCountry);
-//       }
-//     //   else if (filteredDataCountry.length === 0){
-//     //     d3.select("tbody").selectAll("tr").remove();
-//     //     errMsg.append("p").text("These are not the droids you are looking for, move along");
-//     // };
-
-
-//     if (filteredDataShape !== 0) {
-//       populateData(filteredDataShape);
-//       }
-
-//     else {
-//       d3.select("tbody").selectAll("tr").remove();
-//       errMsg.append("p").text("These are not the droids you are looking for, move along");
-//     };
-// }
-
-
-
-
-
-
-
-
-
-
-
-
-// var buttonCity = d3.select("#filter-btn-city");
-
-// buttonCity.on("click", runEnterCity);
-
-
-// function runEnterCity () { 
-
-//     d3.event.preventDefault();
-
-//     var inputElementCity = d3.select("#city");
-
-//     var inputValueCity = inputElementCity.property("value");
-
-//     var filteredDataCity = tableData.filter(tableData => tableData.city === inputValueCity);
-
-//     tbody.html("");
-    
-
-//     // if (filteredDataCity.datetime === inputValueDate) {
-//     //   filteredDataCity.forEach((sightingsCity) => {
-//     //     var row = tbody.append("tr");
-//     //     Object.entries(sightingsCity).forEach(([key, value]) => {
-//     //       var cell = row.append("td");
-//     //       cell.text(value);
-          
-//     //     });
-//     //   });
-//     // }
-//     // else {
-
-//     filteredDataCity.forEach((sightingsCity) => {
-//         var row = tbody.append("tr");
-//         Object.entries(sightingsCity).forEach(([key, value]) => {
-//           var cell = row.append("td");
-//           cell.text(value);
-          
-//         });
-//       });
-//     // }
-// };
-
-// var buttonState = d3.select("#filter-btn-state");
-
-// buttonState.on("click", runEnterState);
-
-
-// function runEnterState () {
-
-//     d3.event.preventDefault();
-
-//     var inputElementState = d3.select("#state");
-
-//     var inputValueState = inputElementState.property("value");
-
-//     var filteredDataState = tableData.filter(tableData => tableData.state === inputValueState);
-
-//     tbody.html("");
-
-//     filteredDataState.forEach((sightingsState) => {
-//         var row = tbody.append("tr");
-//         Object.entries(sightingsState).forEach(([key, value]) => {
-//           var cell = row.append("td");
-//           cell.text(value);
-          
-//         });
-//       });
-// };
-
-// var buttonCountry = d3.select("#filter-btn-country");
-
-// buttonCountry.on("click", runEnterCountry);
-
-
-// function runEnterCountry () {
-
-//     d3.event.preventDefault();
-
-//     var inputElementCountry = d3.select("#country");
-
-//     var inputValueCountry = inputElementCountry.property("value");
-
-//     var filteredDataCountry = tableData.filter(tableData => tableData.country === inputValueCountry);
-
-//     tbody.html("");
-
-//     filteredDataCountry.forEach((sightingsCountry) => {
-//         var row = tbody.append("tr");
-//         Object.entries(sightingsCountry).forEach(([key, value]) => {
-//           var cell = row.append("td");
-//           cell.text(value);
-          
-//         });
-//       });
-// };
-
-
-// var buttonCountry = d3.select("#filter-btn-shape");
-
-// buttonCountry.on("click", runEnterShape);
-
-
-// function runEnterShape () {
-
-//     d3.event.preventDefault();
-
-//     var inputElementShape = d3.select("#shape");
-
-//     var inputValueShape = inputElementShape.property("value");
-
-//     var filteredDataShape = tableData.filter(tableData => tableData.shape === inputValueShape);
-
-//     tbody.html("");
-
-//     filteredDataShape.forEach((sightingsShape) => {
-//         var row = tbody.append("tr");
-//         Object.entries(sightingsShape).forEach(([key, value]) => {
-//           var cell = row.append("td");
-//           cell.text(value);
-          
-//         });
-//       });
-// };
-
-// try change instead of click button and it will allow to switch between parameters.
+}
